a75c07a7510cbb214641be97cd23d9c052c004fb
ChrisZhang <zcyjim@outlook.com>
2021-06-26 01:04:31 +0800

    把前面的东西改得可以过check了

--- src/sceneparser.rs
+++ src/sceneparser.rs
@@ -14,7 +14,7 @@ mod sceneparser {
         let mut json_parsed = json::parse(&json_raw)
             .expect("Json invalid!");
         let camera = json_parsed.remove("Camera");
-        let lights = json_parsed.remove("Lights");
+        let mut lights = json_parsed.remove("Lights");
         let materials = json_parsed.remove("Materials");
         let group = json_parsed.remove("Group");
         assert!(camera.is_object());
@@ -22,9 +22,9 @@ mod sceneparser {
         assert!(materials.is_object());
         assert!(group.is_object());
         let camera = build_camera(camera);
-        let light_vec: Vec<Box<dyn Light>>;
-        for &mut light_attr in lights.members_mut() {
-            light_vec.push(build_light(light_attr));
+        let mut light_vec: Vec<Box<dyn Light>> = vec![];
+        for i in 0.. lights.members().len() {
+            light_vec.push(build_light(lights[i].take()));
         }
         SceneParser {
             camera,
